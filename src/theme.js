5a73ac4076b8fda4f6c2b08322532bb3725b00b8
SmilingWitch <danielitayero@gmail.com>
2024-07-12 16:47:14 -0300

    chore: add theme

---
+++ src/theme.js
@@ -0,0 +1,34 @@
+
+const theme = {
+    colors: {
+        textPrimary: '#464646',  //#586069
+        textSecundary: '#f5f5f5',
+        primary: '#fff',
+        secundary: '#f6632d',
+        white: "#fefefe",
+        light: '80A0B5',
+        grey: '#F6E2D9',
+        lightGrey: '#D2D2D2',
+        cardColor: '#BA9985',
+        buttonColor: '#F2CFC2',
+        deseableColor: 'rgb(190, 187, 187)',
+        container:'#F5F0EE'
+
+    },
+    fontSize: {
+        h1: 32,
+        h2: 24,
+        h3: 18.72,
+        regular: 16,
+        small: 12
+    },
+    fonts: 'System',
+    fontWeight: {
+        normal: '400',
+        bold: '700'
+    },
+    padding: 10
+}
+
+
+export default theme
