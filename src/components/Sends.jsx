ea4acb35399aaacec516bdf6aa63d17fb80cde99
SmilingWitch <danielitayero@gmail.com>
2024-07-12 16:44:47 -0300

    feat: add Sends.jsx

---
+++ src/components/Sends.jsx
@@ -0,0 +1,25 @@
+import { View, StyleSheet } from "react-native"
+import StyledText from "./StyledText"
+import theme from "../theme"
+
+
+const Sends = () => {
+    return(
+        <View style = {styles.container}>
+            <StyledText fontSize="small">No existen solicitudes de envio</StyledText>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        minHeight: 200,
+        backgroundColor: theme.colors.container,
+        padding: 15,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+export default Sends
