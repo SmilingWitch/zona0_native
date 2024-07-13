41cb81bf1db0b4b9eadf910766e96af4603dd118
SmilingWitch <danielitayero@gmail.com>
2024-07-13 20:46:34 -0300

    feat: add styled prop to ReceibeItem

--- src/components/ReceibeItemBtn.jsx
+++ src/components/ReceibeItemBtn.jsx
@@ -1,33 +1,39 @@
-import { TouchableOpacity,StyleSheet } from "react-native"
+import { TouchableOpacity,StyleSheet, TouchableWithoutFeedback } from "react-native"
 import StyledText from "./StyledText"
 import theme from "../theme"
 
 
-const ReceibeItemBtn = ({route, focused}) => {
+const ReceibeItemBtn = ({route, focused, styled}) => {
     return(
-        <TouchableOpacity 
-            
+        <TouchableWithoutFeedback
             onPress={() => console.log(route)} >
-            <StyledText  fontWeight="bold" color="secundary" style={focused ? styles.btn_active : styles.btn} >{route.title}</StyledText>
-        </TouchableOpacity>
+                <StyledText  
+                    fontWeight={focused ? "bold"  : ''}
+                    fontSize='small'
+                    style={styled ? (focused ? styles.btn_active : styles.btn) : (focused ? styles.btn_not_focus : styles.btn)}  >
+                        {route.title}
+                    </StyledText>
+        </TouchableWithoutFeedback>
     )
 }
-
 
 const styles = StyleSheet.create({
     btn: {
         borderRadius: 30,
         alignItems: 'center',
         justifyContent: 'center',
+        color: theme.colors.textPrimary,
     },
     btn_active: {
-        color: theme.colors.secundary,
+        color: theme.colors.textSecundary,
         height: '100%',
         borderRadius: 30,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    btn_not_focus: {
+        color: theme.colors.textPrimary,
     }
-
 })
 
 
