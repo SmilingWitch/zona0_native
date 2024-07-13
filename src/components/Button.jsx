a93908db5286987863f00a7d5bad9dc29a67013a
SmilingWitch <danielitayero@gmail.com>
2024-07-13 20:43:28 -0300

    feat: add horizontal padding to Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ import theme from "../theme"
 const Button = ({text, fnc, ...props}) => {
     return(
         <TouchableOpacity style = {styles.container} onPress={() => fnc}>
-            <StyledText color = "secundary">{text}</StyledText>
+            <StyledText color = "secundary" fontSize='small'>{text.toUpperCase()}</StyledText>
         </TouchableOpacity>
     )
 }
@@ -16,6 +16,7 @@ const styles = StyleSheet.create({
         backgroundColor: theme.colors.secundary,
     width: '100%',
     padding: 15,
+    paddingHorizontal: 20,
     borderRadius: 20,
     alignItems: 'center'
     }
