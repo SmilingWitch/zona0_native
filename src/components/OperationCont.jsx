92ba586c17d001d5c7b31b4f49c321aa60bb0d0a
SmilingWitch <danielitayero@gmail.com>
2024-07-15 20:59:50 -0300

    feat: adapt Operationcont for all operations

--- src/components/OperationCont.jsx
+++ src/components/OperationCont.jsx
@@ -1,22 +1,82 @@
-import { View, StyleSheet, TouchableOpacity } from "react-native"
+import { View, StyleSheet} from "react-native"
 import theme from "../theme"
 import StyledText from "./StyledText"
 import StyledTextInput from "./StyleTextInput"
 import Button from "./Button"
+import { Formik } from "formik"
+import FormikInputValue from "./FormikInputValue"
+import { useState } from "react"
+import { useSelector } from "react-redux"
+import { fetchData } from "../api/authentication/fetchData"
+
+const OperationCont = ({header, 
+                        content,
+                        btn_text,
+                        placeholder, 
+                        initialValues,
+                        name, 
+                        validationScheme,
+                        url,
+                        operation,
+                        navigation}
+                    ) => {
+
+    const [error, setError ] = useState(null)
+    const [loading, setLoading ] = useState(false)
+    const accessToken = useSelector(state => state.accessToken)
+
+    const operationFetch = async (value, url) => {
+        console.log(value)
+        console.log(url)
+        setLoading(true)
+        fetchData(url, value, {"access_token": accessToken})
+        .then(data => {
+                setLoading(false)
+                console.log(data)
+                if(operation === 'receive'){
+                    navigation.navigate("ReceiveDetails", 
+                                        {amount: data.amount,
+                                        code: data.code,
+                                        date: data.date,
+                                        image: data.image,
+                                        id: data.id,
+                                        user: data.user
+                                        })
+                }
+                })
+        .catch(error => {
+                console.log(error)
+                setLoading(false)})
+    }
 
 
-
-const OperationCont = ({header, content, fnc, btn_text,placeholder}) => {
     return(
         <View style = {styles.container}>
             <View style = {styles.bx}>
                 <StyledText  fontWeight="bold">{header}</StyledText>
                 <StyledText fontSize="small">{content}</StyledText>
-                <StyledTextInput placeholder = {placeholder}></StyledTextInput>
+                <Formik initialValues={initialValues} 
+                    onSubmit={value => operationFetch(value ,url)} 
+                    validationSchema ={validationScheme}>
+                    {({handleSubmit}) => (
+                        <View style = {styles.form}>
+                            <View style = {styles.input_bx}>
+                                <FormikInputValue
+                                    placeholder={placeholder} 
+                                    name = {name}
+                                />
+                            </View>
+                            <View style = {styles.error}>
+                                <StyledText fontSize="small" error>{error}</StyledText>
+                            </View>
+                            <View style = {styles.btn_bx}>
+                                <Button fnc ={handleSubmit} text = {btn_text} loading = {loading}></Button>
+                            </View>
+                            
+                        </View>
+                        )}
+                </Formik>
             </View>
-            <View style = {styles.btn_bx}>
-                <Button text = {btn_text} fnc={fnc}/>
-            </View> 
         </View>
     )
 }
@@ -25,20 +85,28 @@ const styles = StyleSheet.create({
     container: {
         padding: theme.padding,
         gap: 30,
-        alignItems: 'center'
+        alignItems: 'center',
     },
     btn_bx:{
         width: 200,
     },
     bx: {
         width: '100%',
-        minHeight: 200,
+        minHeight: 250,
         backgroundColor: theme.colors.container,
         padding: theme.padding,
         flex: 1,
         borderRadius: 20,
         alignItems: 'center',
         justifyContent: 'space-around'
+    },
+    form: {
+        width: '100%',
+        marginTop: 20,
+        alignItems: 'center'
+    },
+    input_bx: {
+        width: '100%'
     }
 })
 
