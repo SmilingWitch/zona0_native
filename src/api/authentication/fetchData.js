a11c1176c4cc8ba8e1cc0f4337b4feb7eef951fb
SmilingWitch <danielitayero@gmail.com>
2024-07-15 20:51:23 -0300

    feat: add fetchData.js

---
+++ src/api/authentication/fetchData.js
@@ -0,0 +1,34 @@
+import { BASE_URL } from '../../../config';
+
+async function fetchData(endpoint, body = null, headers = null) {
+  let options = {
+    method: 'GET', // Método predeterminado
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      headers
+    }
+  };
+
+  // Modificar el método y el cuerpo si se proporcionan
+  if (body !== null) {
+    options.method = 'POST'; // Cambiar a POST u otro método según sea necesario
+    options.body = JSON.stringify(body);
+  }
+
+  try {
+    const response = await fetch(`${BASE_URL}${endpoint}`, options);
+    if (!response.ok) {
+      
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json(); // Utilizar await para obtener el resultado de json()
+  
+  } catch (error) {
+    console.error('Error:', error);
+    throw error; // Re-lanza el error para que pueda ser capturado por el llamador
+  }
+}
+
+export { fetchData };
+
