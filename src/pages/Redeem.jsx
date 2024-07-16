4180e7ece1a01d65701549f4800540331a52e501
SmilingWitch <danielitayero@gmail.com>
2024-07-16 15:22:01 -0300

    feat: add new props to redeem

--- src/pages/Redeem.jsx
+++ src/pages/Redeem.jsx
@@ -2,10 +2,16 @@ import { View,StyleSheet} from "react-native"
 import theme from "../theme"
 import OperationCont from "../components/OperationCont"
 import BackHeader from "../components/BackHeader"
+import { redeemValidationSchema } from "../validationSchemas/redeem"
+
+
 
 
 const Redeem = ({navigation}) => {
 
+    const initialValues = {
+        code: ''
+    }
 
     return(
         <View style = {styles.container}>
@@ -15,6 +21,10 @@ const Redeem = ({navigation}) => {
                 content="Aqui podras canjear codigos que estan disponibles para obtener OSP de manera gratuita"
                 btn_text="Verificar"
                 placeholder="Codigo"
+                initialValues= {initialValues}
+                name= "code"
+                validationScheme= {redeemValidationSchema}
+                url= "/redeem-code/code/redeem/"
                 />
         </View>
     )
