0e7b662e231183fce52d6e0cd32124c3a9e2673c
SmilingWitch <danielitayero@gmail.com>
2024-07-16 15:22:30 -0300

    feat: add new props to Send

--- src/pages/Send.jsx
+++ src/pages/Send.jsx
@@ -2,11 +2,14 @@ import { View, StyleSheet } from "react-native"
 import theme from "../theme"
 import BackHeader from "../components/BackHeader"
 import OperationCont from "../components/OperationCont"
+import { sendValidationSchema } from "../validationSchemas/send"
 
+const initialValues = {
+    code: ""
+}
 
-const Send = ({navigation
-    
-}) => {
+const Send = ({navigation}) => {
+
     return(
         <View style = {styles.container}>
             <BackHeader navigation = {navigation} name = "Enviar OSP"/>
@@ -15,6 +18,12 @@ const Send = ({navigation
                 content="Introduzca el codigo para proceder a pagar el monto establecido"
                 btn_text="Verificar"
                 placeholder="Codigo"
+                name = "code"
+                url = "/transfer/create-receive/"
+                validationScheme={sendValidationSchema}
+                initialValues={initialValues}
+                navigation = {navigation}
+                operation="send"
                 />
         </View>
         
