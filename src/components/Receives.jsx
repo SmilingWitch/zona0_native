3d5a5cd008c5b2efced6380f76ab3a3cebd97087
SmilingWitch <danielitayero@gmail.com>
2024-07-16 22:43:31 -0300

    feat: add Receives.jsx

---
+++ src/components/Receives.jsx
@@ -0,0 +1,24 @@
+import { View, StyleSheet } from "react-native"
+import TabViewComponent from "./TabViewComponent"
+import theme from "../theme"
+
+
+const Receives = ({routesComponent}) => {
+    return (
+        <View style = {styles.container}>
+            <TabViewComponent 
+                routesComponent ={routesComponent}
+                />
+
+        </View>)
+}
+
+const styles = StyleSheet.create({
+    container: {
+        minHeight: 300,
+        padding:15,
+        borderRadius: 15,
+        backgroundColor: theme.colors.container,
+    },
+})
+export default Receives
