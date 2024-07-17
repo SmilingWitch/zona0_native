52446d89848534c6473c3a0a15ea5064cc6adcba
SmilingWitch <danielitayero@gmail.com>
2024-07-17 16:19:46 -0300

    feat: validate that the code has the correct structure in send schema

--- src/validationSchemas/send.js
+++ src/validationSchemas/send.js
@@ -1,9 +1,8 @@
 import * as Yup from 'yup';
 
-
-
-export  const sendValidationSchema = Yup.object({
+export const sendValidationSchema = Yup.object({
     code: Yup
         .string()
-        .required('Is required')
+        .matches(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i, 'Invalid code')
+        .required('Is required'),
 });
