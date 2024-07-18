531edb1da8eb307400148f44766f8ff1aedf5db2
SmilingWitch <danielitayero@gmail.com>
2024-07-18 17:16:55 -0300

    feat: add common/

---
+++ src/components/common/FormikInputValue.jsx
@@ -0,0 +1,43 @@
+import { useField } from "formik"
+import { View, StyleSheet } from "react-native"
+import StyledTextInput from "./StyleTextInput"
+import StyledText from "./StyledText"
+
+const FormikInputValue = ({name, ...props}) => {
+    
+    const [field, meta, helpers] = useField(name)
+
+
+    return(
+        <View style = {styles.input_container}>
+            <StyledTextInput
+                error = {meta.touched && meta.error ? meta.error : ''}
+                value={field.value} 
+                onChangeText={value => helpers.setValue(value)} 
+                onBlur={() => helpers.setTouched(true)}
+                {...props}/>
+
+            <View style = {styles.errorContainer}>
+                {meta.touched && meta.error ? (
+                    <StyledText style={styles.error} fontSize='small'>{meta.error}</StyledText>
+                ) : null}
+
+            </View>
+
+        </View>
+        
+    )
+}
+
+
+const styles = StyleSheet.create({
+    input_container: {
+        width: '100%'
+    },
+  error: {
+    color: 'red'
+  }
+   });
+
+
+export default FormikInputValue
