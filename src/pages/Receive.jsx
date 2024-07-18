71130703802e1dfd36bcc6323f7cce291306ae33
SmilingWitch <danielitayero@gmail.com>
2024-07-18 17:18:32 -0300

    ref: change the name of Receibe for Receive

---
+++ src/pages/Receive.jsx
@@ -0,0 +1,43 @@
+import { View,StyleSheet} from "react-native"
+import theme from "../theme"
+import OperationCont from "../components/common/OperationCont"
+import BackHeader from "../components/common/BackHeader"
+import { receiveValidationSchema } from "../validationSchemas/receive"
+
+
+const initialValues = {
+    amount: ""
+}
+
+const Receive = ({navigation}) => {
+
+    return(
+        <View style = {styles.container}>
+            <BackHeader name = "Recibir SOP" navigation = {navigation}/>
+            <OperationCont 
+                header = "Entre el monto" 
+                content="Introduzca la cantidad de OSP que desea recibir para generar un codigo de pago"
+                btn_text="Generar codigo"
+                placeholder="Monto a recibir"
+                name = "amount"
+                url = "/transfer/create-receive/"
+                validationScheme = {receiveValidationSchema}
+                initialValues={initialValues}
+                navigation = {navigation}
+                operation="receive"
+                />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.colors.primary,
+    },
+
+    
+})
+
+
+export default Receive
