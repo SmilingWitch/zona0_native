628cb683e6ef2f331c2997ddf0508768f5595864
SmilingWitch <danielitayero@gmail.com>
2024-07-18 14:48:50 -0300

    feat: add options to Menu

--- src/components/settings/Menu.jsx
+++ src/components/settings/Menu.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet} from "react-native"
+import { View, StyleSheet, Image, DrawerLayoutAndroid,} from "react-native"
 import StyledText from "../StyledText"
 import theme from "../../theme"
 import Button from "../Button"
@@ -6,9 +6,7 @@ import { fetchData } from "../../api/authentication/fetchData"
 import { useDispatch, useSelector } from "react-redux"
 import { useState } from "react"
 import { logout } from "../../store/reducer"
-
-
-
+import BackHeader from "../BackHeader"
 
 
 const Menu = ({navigation}) => {
@@ -17,6 +15,7 @@ const Menu = ({navigation}) => {
     const accessToken = useSelector(state => state.accessToken)
     const dispatch = useDispatch()
     const [loading, setLoaging] = useState(false)
+    const user = useSelector(state => state.user)
 
     const logout_session = () => {
         setLoaging(true)
@@ -34,12 +33,36 @@ const Menu = ({navigation}) => {
     }
 
 
+
     return(
         <View style = {styles.container}>
-            
-            <Button text = "Logout" fnc = {logout_session}  loading={loading}>
+            <BackHeader navigation = {navigation} name = "Settings"></BackHeader>
+            <View style = {styles.bx}>
+                <View style = {styles.header}>
+                    <StyledText fontSize="h3">Operations</StyledText>
+                </View>
+                <View style = {styles.user_info}>
+                    <Image 
+                        source={require('../../../assets/images/fondo.jpg')}
+                        style = {styles.image}></Image>
+                        <View style = {styles.user_details}>
+                            <StyledText fontWeight='bold'>{`${user.name} ${user.last_name}`}</StyledText>
+                            <StyledText fontWeight='bold'>{user.username}</StyledText>
+                        </View>
+                    
+                </View>
+                <View style = {styles.btns_bx}>
+                    <Button text = "Ver Perfil"  loading={loading}/>
+                    <Button text = "Editar datos"   loading={loading}/>
+                    <Button text = "Gestionar Tarjeta"  loading={loading}/>
+                    <Button text = "Cambiar contrasena"  loading={loading}/>
+                    <Button text = "Cerrar Sesion" fnc = {logout_session}  loading={loading}/>
+                    <Button text = "Eliminar cuenta"  loading={loading}/>
+                </View>
+                
+            </View>  
 
-            </Button>
+  
         </View>
         
     )
@@ -49,6 +72,38 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: theme.colors.white
+    },
+    bx: {
+        padding: theme.padding,
+        gap: 15
+    },
+    header:{
+        borderStartWidth: 4,
+        borderColor: theme.colors.secundary,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 5,
+        paddingVertical: 5,
+        alignItems: 'center'
+    },
+    user_info: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 20,
+        gap: 20,
+        backgroundColor: theme.colors.buttonColor,
+        borderRadius: 20
+    },
+    image: {
+        width: 80,
+        height: 80,
+        borderRadius: 2000,
+    },
+    user_details: {
+
+    },
+    btns_bx: {
+        gap: 5
     }
 })
 
