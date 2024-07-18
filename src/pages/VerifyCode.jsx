b8f636c2504609f73bf484f4e92073c5b67e7d91
SmilingWitch <danielitayero@gmail.com>
2024-07-18 14:50:55 -0300

    feat: add verifyTokenVaidationScheme to VerifyToken

--- src/pages/VerifyCode.jsx
+++ src/pages/VerifyCode.jsx
@@ -1,25 +1,43 @@
+import { View, StyleSheet } from "react-native"
 import OperationCont from "../components/OperationCont"
-import StyledText from "../components/StyledText"
 import { redeemValidationSchema } from "../validationSchemas/redeem"
+import theme from "../theme"
+import { verifyTokenValidationSchema } from "../validationSchemas/verifyToken"
 
-const VerifyCode = () => {
+const VerifyCode = ({navigation}) => {
 
     const initialValues = {
-        code: ''
+        token: ''
     }
 
 
+
+
     return( 
-    <OperationCont
+    <View style = {styles.container}>
+        <OperationCont
                 header = "Enytre el codigo"
                 content= "Enviamos un codigo de verificacion temporal a su correo. Copie el codigo aqui para verificar su registro"
                 btn_text="verificar"
                 placeholder="Codigo"
-                initialValues={ initialValues}
-                name = "code"
-                validationScheme={redeemValidationSchema}
-                url = ""
+                initialValues={initialValues}
+                name = "token"
+                validationScheme={verifyTokenValidationSchema}
+                url = {null}
+                operation = "verify_token"
+                navigation = {navigation}
     />
+
+    </View>
+    
 )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: theme.padding,
+        backgroundColor: theme.colors.white
+    }
+})
 export default VerifyCode
