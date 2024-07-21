1d28ec1b5a761c1d6a9ed390a5a48a50b98b7cf4
SmilingWitch <danielitayero@gmail.com>
2024-07-21 15:04:36 -0300

    ref: if response isn't ok do nothing in fetchData

--- src/api/authentication/fetchData.js
+++ src/api/authentication/fetchData.js
@@ -15,17 +15,12 @@ async function fetchData(endpoint, body = null, headers = null, logout) {
     options.method = 'POST'; // Cambiar a POST u otro método según sea necesario
     options.body = JSON.stringify(body);
   }
-
   try {
     const response = await fetch(`${BASE_URL}${endpoint}`, options);
-    /*if (!response.ok) {
-      
-      throw response.status;
-    }*/
     return await response.json(); // Utilizar await para obtener el resultado de json()
   
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error:' ,error);
     throw error; // Re-lanza el error para que pueda ser capturado por el llamador
   }
 }
