68c527616c34fcb14051ddc788d4601a78cf771c
SmilingWitch <danielitayero@gmail.com>
2024-07-21 15:44:13 -0300

    ref: delete useEffect in PendingList

--- src/components/principal_page/PendingList.jsx
+++ src/components/principal_page/PendingList.jsx
@@ -1,38 +1,15 @@
 import StyledText from "../common/StyledText"
 import { View, StyleSheet } from "react-native"
-import { useEffect, useState } from "react"
-import { fetchData } from "../../api/authentication/fetchData"
-import { useDispatch, useSelector } from "react-redux"
-import { setpendingList } from '../../store/reducer';
+import {useSelector } from "react-redux"
 import ReceiptsLastOperationItemList from "./ReceiptsLastOperationItemList"
 
 const PendingList = ({navigation}) => {
-   
-const accessToken = useSelector(state => state.accessToken)
-const [error, setError] = useState(null)
-const dispatch = useDispatch();
-const pendingList = useSelector(state => state.pendingList)
 
+    const pendingList = useSelector(state => state.pendingList)
 
-useEffect( () => {
-    fetchData(
-        "/transfer/list-unpaid-receive/", 
-        null,
-        {"access_token": accessToken} )
-        .then(data => {
-            if(data !== pendingList){
-                dispatch(setpendingList(data))
-            }
-            setError(null)
-            
-        })
-        .catch(error => {
-            setError(error)
-        })
-},[])
 
     return(
-        pendingList.message  ?
+        pendingList.message || !pendingList  ?
             <View style = {styles.empty_container}>
                 <StyledText fontSize='small'>{pendingList.message}</StyledText>
             </View>
