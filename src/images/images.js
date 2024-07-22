4eea3f5722208502c55e1845125aacb9d46744c7
SmilingWitch <danielitayero@gmail.com>
2024-07-22 13:16:18 -0300

    feat: add images/

---
+++ src/images/images.js
@@ -0,0 +1,18 @@
+import image1 from "../../assets/images/people.png"
+import image2 from "../../assets/images/pen.png"
+import image3 from "../../assets/images/credit-card.png"
+import image4 from "../../assets/images/padlock.png"
+import image5 from "../../assets/images/exit.png"
+import image6 from "../../assets/images/delete.png"
+
+
+
+
+export default {
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+};
