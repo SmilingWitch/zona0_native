ad23662986ad9ddd48cd8726e48a2e593fe9d651
SmilingWitch <danielitayero@gmail.com>
2024-07-22 16:25:33 -0300

    feat: add total_balance option to operations

--- src/api/authentication/operations.js
+++ src/api/authentication/operations.js
@@ -1,12 +1,19 @@
 import { fetchData } from "./fetchData"
 
 
-export const operations = (accessToken, dispatch, url, reducer) => fetchData(
+export const operations = (accessToken, dispatch, url, reducer, operation) => fetchData(
     url, 
     null,
     {"access_token": accessToken} )
     .then(data => {
-        dispatch(reducer(data))
+        if(operation === 'total_balance'){
+            dispatch(reducer(data.orcaStore_point))
+        }else{
+            dispatch(reducer(data))
+        }
+        
+        console.log("OPERATION",operation)
+        
     })
     .catch(error => {
         console.log("ERRORRRR",error)
