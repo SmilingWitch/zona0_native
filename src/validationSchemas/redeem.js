4794de82cc228a77ab6959ebdb71dccc9c2e88c0
SmilingWitch <danielitayero@gmail.com>
2024-07-22 16:36:26 -0300

    feat: add validation to redeem validationSchemas

--- src/validationSchemas/redeem.js
+++ src/validationSchemas/redeem.js
@@ -5,5 +5,6 @@ import * as Yup from 'yup';
 export  const redeemValidationSchema = Yup.object({
     code: Yup
         .string()
+        .matches(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i, 'Invalid code')
         .required('Is required')
 });
