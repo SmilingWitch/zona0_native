a991fdec4be563d719545545ad15a6c4d475eb51
SmilingWitch <danielitayero@gmail.com>
2024-07-23 18:17:55 -0300

    feat: add darkTheme.js

---
+++ src/darkTheme.js
@@ -0,0 +1,33 @@
+const darkTheme = {
+    colors: {
+        textPrimary: '#f5f5f5',
+        textSecondary: '#464646',
+        primary: '#000',
+        secundary: '#E3C0B0',
+        white: "#0B0A09",
+        light: '#80A0B5',
+        grey: '#333',
+        lightGrey: '#444',
+        cardColor: '#555',
+        buttonColor: '#6C4F44',
+        desirableColor: 'rgb(80, 80, 80)',
+        container: "rgba(40,20,3, 0.7)"/*"#1F2328"*/,
+        dialogColor: "#6C4F44"
+    },
+    fontSize: {
+        h1: 32,
+        h2: 24,
+        h3: 18.72,
+        regular: 16,
+        small: 12
+    },
+    fonts: 'System',
+    fontWeight: {
+        normal: '400',
+        bold: '700'
+    },
+    padding: 10
+};
+
+
+export default darkTheme
