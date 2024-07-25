d984da8111a188afb5dfe2ea7426dad7d71a4340
SmilingWitch <danielitayero@gmail.com>
2024-07-25 17:17:27 -0300

    feat: disable the option to back up in the principal page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,10 +2,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../api/authentication/fetchData";
 import StyledText from "../components/common/StyledText"
 import { setDonatedList, setpendingList, setPerformedList, setTransferedList,setBankedList } from "../store/reducer";
-import { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { useCallback, useEffect, useState } from "react";
+import { View, StyleSheet, BackHandler } from "react-native";
 import theme from "../theme";
 import { operations } from "../api/authentication/operations";
+import { useFocusEffect } from "@react-navigation/native";
 
 
 
@@ -13,6 +14,24 @@ const Welcome = ({navigation}) => {
 
     const dispatch = useDispatch();
     const accessToken = useSelector(state => state.accessToken)
+
+
+    useFocusEffect(
+        useCallback(() => {
+          const onBackPress = () => {
+            // Si retorna true, previene el comportamiento predeterminado de retroceso
+            return true;
+          };
+    
+          // Agregar el listener para el evento de retroceso del hardware
+          BackHandler.addEventListener('hardwareBackPress', onBackPress);
+    
+          return () => {
+            // Remover el listener cuando el componente se desenfoca
+            BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+          };
+        }, [])
+      );
 
 
     useEffect(() => {
