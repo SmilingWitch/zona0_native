e973c729cd460547fc571b62ea2e1524a325d029
SmilingWitch <danielitayero@gmail.com>
2024-07-25 15:16:29 -0300

    feat: add OperationCont to BankingOperation

--- src/components/operations/BankingOpperation.jsx
+++ src/components/operations/BankingOpperation.jsx
@@ -6,68 +6,50 @@ import theme from "../../theme"
 import Icon from '@expo/vector-icons/EvilIcons'
 import { useSelector } from "react-redux"
 import darkTheme from "../../darkTheme"
+import OperationCont from "../common/OperationCont"
+import { bankingrValidationSchema } from "../../validationSchemas/banking"
 
-const BankingOpperation = () => {
+
+
+
+const BankingOpperation = ({navigation}) => {
 
     const theme1 = useSelector(state => state.darkTheme)
     const styles = getStyles(theme1 ? theme : darkTheme )
+    const user = useSelector(state => state.user)
+
+    const initialValues = {
+        amount:'',
+        user: user.username ? user.username : '',
+        state: ''
+      }
 
     return(
         <View style = {styles.container}>
-            <View style = {styles.bx}>
-                <StyledText  fontWeight="bold">Entra el monto</StyledText>
-                <StyledText fontSize="small">
-                        Deposita tus fondos y obten un interes fijo del 3% cada 30 dias para hacer crecer tus ahorros. 
-                        Puedes hacer uso de la calculadora para tener una idea mas precisa de la ganancia que puede llegar a obtener.</StyledText>
-                
-                <View>
-                    <Button text = "Calculadora"></Button>
-                </View>
-                <StyledText fontSize='small' color='resalt'>
+            <OperationCont 
+                header = "Entre el monto" 
+                content=" Deposita tus fondos y obten un interes fijo del 3% cada 30 dias para hacer crecer tus ahorros."
+                btn_text="Depositar"
+                placeholder="Monto a recibir"
+                name = "amount"
+                url = "/banking/account/"
+                validationScheme = {bankingrValidationSchema}
+                initialValues={initialValues}
+                navigation = {navigation}
+                operation="banking"
+                />
+                <StyledText fontSize='small' color='resalt' style = {styles.text}>
                     Los OSP depositados quedaran congelados un total de 60 dias. Luego de eso podra retirarlos
                 </StyledText>
-                
-                <StyledTextInput placeholder = "monto"></StyledTextInput>
-            </View>
-            <View style = {styles.btn_bx}>
-                <Button text = "Depositar" /*fnc={fnc}*//>
-            </View> 
         </View>
     )
 }
 
 const getStyles = (theme) => StyleSheet.create({
-    container: {
-        padding: theme.padding,
-        gap: 30,
-        alignItems: 'center'
+    text: {
+        padding: theme.padding
     },
-    btn_bx:{
-        width: 200,
-    },
-    bx: {
-        width: '100%',
-        minHeight: 250,
-        backgroundColor: theme.colors.container,
-        padding: theme.padding,
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        gap: 15
-    },
-    header:{
-        borderStartWidth: 4,
-        borderColor: theme.colors.secundary,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingLeft: 5,
-        paddingVertical: 5,
-        alignItems: 'center',
-        width: '100%'
-    },
-    icon: {
-        fontSize: theme.fontSize.h1
-    }
+    
 })
 
 export default BankingOpperation
