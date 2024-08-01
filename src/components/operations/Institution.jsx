4292ec0d2328a65f696c6930d6f05d87530e27d7
SmilingWitch <danielitayero@gmail.com>
2024-08-01 18:53:09 -0300

    feat: add Institution

---
+++ src/components/operations/Institution.jsx
@@ -0,0 +1,51 @@
+
+import { useSelector } from "react-redux"
+import StyledText from "../common/StyledText"
+import Button from "../common/Button"
+import { View,  StyleSheet, Image } from "react-native"
+import theme from "../../theme"
+import darkTheme from "../../darkTheme"
+
+
+const Institution = ({item, navigation}) => {
+
+    const theme1 = useSelector(state => state.darkTheme)
+    const styles = getStyles(theme1 ? theme : darkTheme );
+
+
+    return(
+        <View style = {styles.container}>
+            <Image source = {{uri: item.image}} style = {styles.image}></Image>
+            <View style = {styles.visit}>
+                <StyledText fontSize='h2' fontWeight='bold'>{item.institution_name}</StyledText>
+                <Button text = "Visit" fnc = {() => navigation.navigate('Institution', {item : item })}></Button>
+            </View>
+        </View>
+        
+    )
+}
+
+
+const getStyles = (theme) => StyleSheet.create({
+    container: {
+        backgroundColor: theme.colors.container,
+        height: 300,
+        borderRadius: 20,
+        marginBottom: 20,
+        overflow: 'hidden'
+    },
+    image: {
+        width: '100%',
+        height: '60%'
+    },
+    visit: {
+        padding: 15,
+        alignItems: 'center',
+        justifyContent: "space-between",
+        height: `40%`
+    }
+})
+
+
+
+export default Institution
